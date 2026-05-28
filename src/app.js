c2feb6c8c38b056e95e4c8687cbdac8f2128fda5
nithin-code-web <nithinbudime@gmail.com>
2026-05-28 19:51:45 +0530

    Initial backend setup with Express server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "GitHub Profile Analyzer API Running"
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port http://localhost:${PORT}`);
+});
